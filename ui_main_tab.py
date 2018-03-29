59b3972cdf1ff8f5ef4e4b644f26fcef8d39e20e
heliomaster <luigidalmaso@gmail.com>
2018-03-29 18:36:23 +0200

    logo avec main_tab

--- ui_main_tab.py
+++ ui_main_tab.py
@@ -5,6 +5,7 @@ from PyQt5.QtSql import *
 from datetime import datetime
 
 import sys
+import time
 import os
 
 import TabView
@@ -90,8 +91,33 @@ class MainDialog(QDialog, TabView.Ui_Dialog):
 if __name__ == '__main__':
     try:
         app = QApplication(sys.argv)
+
+        # create and display splash screen
+
+        splash_pix = QPixmap('Logo_armee.png')
+
+        splash = QSplashScreen(splash_pix, Qt.WindowStaysOnTopHint)
+        splash.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
+        splash.setEnabled(False)
+        # add progress bar
+        progressBar = QProgressBar(splash)
+        progressBar.setMaximum(10)
+        progressBar.setGeometry(0, splash_pix.height() - 50, splash_pix.width(), 20)
+
+        splash.show()
+        splash.showMessage("<h1><font color='black'>----Bienvenue dans Moise!----</font></h1>",
+                           Qt.AlignTop | Qt.AlignCenter, Qt.black)
+
+        for i in range(1, 11):
+            progressBar.setValue(i)
+            t = time.time()
+            while time.time() < t + 0.1:
+                app.processEvents()
+        # simulating
+        time.sleep(1)
         form = MainDialog()
         form.show()
+        splash.finish(form)
         app.exec_()
         sys.exit(0)
     except NameError:
